b003a7825157060ae6b55f03ce04fc70abf18a67
andrei.tazetdinov <andrei.tazetdinov@dynatrace.com>
2020-11-26 17:20:26 +0100

    applicatin with Quasar

---
+++ src/pages/PopupPage.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+     <a href="/www/index.html#/options" target="_blank">Popup</a> 
+ </div>
+</template>
+
+<script lang="ts">
+import ExampleComponent from 'components/ClassComponent.vue';
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  components: { ExampleComponent },
+})
+export default class PopupPage extends Vue {}
+</script>
